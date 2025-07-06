725244bea2923b62aff655a4e92c4f7be5769c15
Lakshmi Venkat <lakshmivenkat082003@gmail.com>
2025-07-06 16:32:40 +0530

    created a method in duplicate class

--- rules/Duplicate.java
+++ rules/Duplicate.java
@@ -1,5 +1,7 @@
 package business_rules_engine.rules;
 import business_rules_engine.order.Order;
 public class Duplicate {
-    
+    public void apply(Order order){
+        System.out.println("Generated a duplicate packing slip for royatlty department for"+order.getType());
+    }
 }
