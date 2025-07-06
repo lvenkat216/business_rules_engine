02650d25c5c4cd75829a9fac7faa01aefc29c219
Lakshmi Venkat <lakshmivenkat082003@gmail.com>
2025-07-06 16:30:45 +0530

    created a method in commision class

--- rules/Commision.java
+++ rules/Commision.java
@@ -1,5 +1,7 @@
 package business_rules_engine.rules;
 import business_rules_engine.order.Order;
 public class Commision {
-    
+    public void apply(Order order){
+        System.out.println("Generated a commison payment for"+order.getType());
+    }
 }
