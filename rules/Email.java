a4c5208f4724b876d55aa00acc5226ab7f8188bc
Lakshmi Venkat <lakshmivenkat082003@gmail.com>
2025-07-06 16:34:56 +0530

    created a method in email class

--- rules/Email.java
+++ rules/Email.java
@@ -2,5 +2,7 @@ package business_rules_engine.rules;
 import business_rules_engine.order.Order;
 
 public class Email {
-    
+    public void apply(Order order){
+        System.out.println("Sent an email regarding"+order.getType()+"to"+order.getEmail());
+    }
 }
