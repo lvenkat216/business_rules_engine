5d219473b4b181c57365c72f82627967611a4db2
Lakshmi Venkat <lakshmivenkat082003@gmail.com>
2025-07-06 16:36:15 +0530

    created a method in upgrademember class

--- rules/UpgradeMember.java
+++ rules/UpgradeMember.java
@@ -1,5 +1,7 @@
 package business_rules_engine.rules;
 import business_rules_engine.order.Order;
 public class UpgradeMember {
-    
+    public void apply(Order order){
+        System.out.println("Upgradedd membership");
+    }
 }
