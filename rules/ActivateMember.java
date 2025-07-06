eace2b3518312d83043760b2228207848f935dab
Lakshmi Venkat <lakshmivenkat082003@gmail.com>
2025-07-06 16:28:02 +0530

    created a method in activatemembership

--- rules/ActivateMember.java
+++ rules/ActivateMember.java
@@ -1,5 +1,7 @@
 package business_rules_engine.rules;
 import business_rules_engine.order.Order;
 public class ActivateMember {
-    
+    public void apply(Order order){
+        System.out.println("Activated membership");
+    }
 }
