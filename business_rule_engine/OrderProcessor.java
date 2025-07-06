4e5da17ff6f0f5db388aa6ce701ffd589a9ee1d0
Lakshmi Venkat <lakshmivenkat082003@gmail.com>
2025-07-06 18:02:33 +0530

    fixed bugs

--- business_rule_engine/OrderProcessor.java
+++ business_rule_engine/OrderProcessor.java
@@ -3,31 +3,31 @@ import business_rules_engine.order.Order;
 import business_rules_engine.rules.*;
 public class OrderProcessor{
     public void process(Order order){
-        if(order.getType().equals("physical")){
+        if(order.getType().equalsIgnoreCase("physical")){
             ShippingPackingSlip ob = new ShippingPackingSlip();
             Commision ob2 = new Commision();
             ob.apply(order);
             ob2.apply(order);
         }
-        if(order.getType().equals("book")){
+        if(order.getType().equalsIgnoreCase("book")){
             Duplicate ob = new Duplicate();
             Commision ob2 = new Commision();
             ob.apply(order);
             ob2.apply(order);
         }
-        if(order.getType().equals("membership")){
+        if(order.getType().equalsIgnoreCase("membership")){
             ActivateMember ob = new ActivateMember();
             Email ob2 = new Email();
             ob.apply(order);
             ob2.apply(order);
         }
-        if(order.getType().equals("upgrade")){
+        if(order.getType().equalsIgnoreCase("upgrade")){
             UpgradeMember ob = new UpgradeMember();
             Email ob2 = new Email();
             ob.apply(order);
             ob2.apply(order);
         }
-        if(order.getName().equals("learning to ski")){
+        if(order.getName().equalsIgnoreCase("learning to ski")){
             Video ob = new Video();
             ob.apply(order);
         }
