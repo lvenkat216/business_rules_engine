65589ccdc7e5aa1dcd48ed69f9f68cd6fbd2f601
Lakshmi Venkat <lakshmivenkat082003@gmail.com>
2025-07-06 16:39:54 +0530

    created a method in video class

--- rules/Video.java
+++ rules/Video.java
@@ -1,5 +1,7 @@
 package business_rules_engine.rules;
 import business_rules_engine.order.Order;
 public class Video{
-    
+    public void apply(Order order){
+        System.out.println("Added free First Aid video to the packing slip");
+    }
 }
